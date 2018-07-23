a0b9bd64cebf6d9f9b1997900dbd9f1b41d5fa48
Paul Müller <dev@craban.de>
2018-07-23 18:11:25 +0200

    tests: minor improvements

--- tests/test_mie.py
+++ tests/test_mie.py
@@ -18,7 +18,8 @@ def test_field2ap_corr():
     phase = np.ones((50, 50)) * .21
     field = np.exp(1j*phase)
     assert np.allclose(phase, mod_mie.field2ap_corr(field)[1])
-    assert np.allclose(phase, mod_mie.field2ap_corr(field * np.exp(2j * np.pi))[1])
+    assert np.allclose(phase, mod_mie.field2ap_corr(
+        field * np.exp(2j * np.pi))[1])
 
 
 if __name__ == "__main__":
